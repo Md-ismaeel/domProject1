9844ca3eeb4aabc27e5a27619013b1a581d0a586
Md Ismail <mdismaeelkhan345@gmail.com>
2024-01-10 00:02:08 +0530

    Updation

---
+++ index.js
@@ -0,0 +1,11 @@
+let css = document.getElementById("box");
+
+css.style.width = "400px";
+css.style.backgroundColor = "yellow";
+css.style.margin = "20px";
+css.style.padding = "10px";
+css.style.fontSize = "18px";
+css.style.fontWeight = "bold";
+css.style.color = "blue";
+css.style.height = "200px";
+css.style.width = "300px";
